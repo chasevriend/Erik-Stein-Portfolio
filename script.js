1a5c9aaf4056140fb6ca9f7b12b2cce65f7a7d67
Chase <chasevriend84@gmail.com>
2023-02-12 12:08:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let btnBefore = document.getElementById("beforebtn");
+let btnAfter = document.getElementById("afterbtn");
+let imgBefore = document.getElementById("beforeimg");
+let imgAfter = document.getElementsById("afterimg");
+
+function replacePicture() {
+    let sourceOfPicture
+}
